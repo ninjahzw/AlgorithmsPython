571a77773feee58184a0bb673762b317638a8f93
houzhaowei <ninja.hzw@gmail.com>
2015-02-22 22:38:01 -0500

    new

--- ml/hw2/OnlinePerceptron.py
+++ ml/hw2/OnlinePerceptron.py
@@ -4,14 +4,19 @@ class classifier:
     def __init__(self):
         self.train_features = 'files/rcv1.train.features'
         self.train_labels = 'files/rcv1.train.labels'
+        self.test_features = 'files/rcv1.test.features'
+        self.test_labels = 'files/rcv1.test.labels'
         self.train_data = []
         self.labels_data = []
         self.terms = 47236
+        # initialize w and b
+        self.w = [0 for x in xrange(self.terms)]
+        self.b = 0
     
-    def load_train_data(self):
+    def load_train_data(self, fname):
         prev = '1'
         row = []
-        with open(self.train_features) as f:
+        with open(fname) as f:
             for line in f:
                 items = line.strip().split(' ')
                 rid = items[0]
@@ -24,25 +29,34 @@ class classifier:
                 row.append(feature)
             self.train_data.append(row)
 
-    def load_train_labels(self):
-        with open(self.train_labels) as f:
+    def load_train_labels(self, fname):
+        with open(fname) as f:
             for line in f:
                 self.labels_data.append(int(line.strip()))
     
     def train(self):
         error = 0
-        # initialize w and b
-        w = [0 for x in xrange(self.terms)]
-        b = 0
         # load data
-        self.load_train_data()
-        self.load_train_labels()
+        self.load_train_data(self.train_features)
+        self.load_train_labels(self.train_labels)
         for i, x in enumerate(self.train_data):
             y = int(self.labels_data[i])
-            dp = self.dot_product(x, w)
-            if y * (self.dot_product(x, w) + b) <= 0:
-                self.improve_w(w, y, x)
-                b += y
+            dp = self.dot_product(x, self.w)
+            if y * (self.dot_product(x, self.w) + self.b) <= 0:
+                self.improve_w(self.w, y, x)
+                self.b += y
+                error += 1
+        return error
+    
+    def test(self):
+        error = 0
+        # load data
+        self.load_train_data(self.test_features)
+        self.load_train_labels(self.test_labels)
+        for i, x in enumerate(self.train_data):
+            y = int(self.labels_data[i])
+            dp = self.dot_product(x, self.w)
+            if y * (self.dot_product(x, self.w) + self.b) <= 0:
                 error += 1
         return error
 
@@ -61,6 +75,7 @@ class classifier:
 
 c = classifier()
 print c.train()
+print c.test()
 #print len(c.train_data), len(c.labels_data)
 #print c.train_data[1]
 #print c.train_data[-1]
